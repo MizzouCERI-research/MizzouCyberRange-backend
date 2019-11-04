26c4da3fd2619c435a85ec479136dc623df94d46
wangso <wangso@missouri.edu>
2019-11-04 15:45:02 -0600

    Create omni_create_sliver.sh

---
+++ omni_create_sliver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# $1 is slice name, $2 is lab module name
+
+aggregate[0]="illinois-ig"
+aggregate[1]="cornell-ig"
+aggregate[2]="umkc-ig"
+aggregate[3]="kansas-ig"
+
+size=${#aggregate[@]}
+index=$(($RANDOM % $size))
+echo ${aggregate[$index]}
+
+function checkErr() {
+    echo -e "$1 failed\". Exiting...">&2; exit;
+}
+
+omni -a ${aggregate[$index]} createsliver $1 /data/CyberRange_$2_rspec_${aggregate[$index]}.xml || checkErr "create sliver "
