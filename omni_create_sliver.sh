f947ab774e0da4d50c17a8954775ed752e9bd427
wangso <wangso@missouri.edu>
2019-11-04 16:09:25 -0600

    Update omni_create_sliver.sh

--- omni_create_sliver.sh
+++ omni_create_sliver.sh
@@ -10,6 +10,13 @@ size=${#aggregate[@]}
 index=$(($RANDOM % $size))
 echo ${aggregate[$index]}
 
+#check to see if there are 2 arguments attached to omni command
+if [ "$#" -lt 2 ]; then
+    echo -e "Not enough arguments supplied, please provide 2 arguments. Exiting..."
+    exit
+fi
+
+#check to see if omni command has succeeded or not
 function checkErr() {
     #echo -e "$1 failed\". Exiting...">&2; exit;
     echo -e "{\"status\":\"fail\"}">&2; exit;
