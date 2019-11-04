75a82f9bf96f6a544cb3e0cd88ccd7cf90853f87
wangso <wangso@missouri.edu>
2019-11-04 15:31:46 -0600

    Update and rename omni-script.sh to omni_create_slice.sh

--- omni-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-omni -a illinois-ig createslice rspecTest
-# we will activate this command once a user is doing this step
-omni addslicemember rspecTest kvhg2 MEMBER
-omni -a illinois-ig createsliver rspecTest /data/CyberRange_all_node_Rspec_ver2.xml
---
+++ omni_create_slice.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+omni createslice $2
+omni addslicemember $2 $1 MEMBER
