196fb72a6e1368ef16351165aa077e18f74ffbf7
wangso <wangso@missouri.edu>
2019-11-12 17:12:12 -0600

    working update

--- omni_create_sliver.sh
+++ omni_create_sliver.sh
@@ -3,6 +3,7 @@
 # $2 is lab module name
 # $3 is user_id
 # $4 is module id
+# $5 is sql file
 
 aggregate[0]="illinois-ig"
 aggregate[1]="cornell-ig"
@@ -13,10 +14,23 @@ size=${#aggregate[@]}
 index=$(($RANDOM % $size))
 echo ${aggregate[$index]}
 
+   source ../db_credential
+   user_id=$3
+   slice_name=$1
+   module_id=$4
+   sh_name="create sliver"
+   sql_query=$5
+
+   echo $user_id
+   echo $slice_name
+   echo $module_id
+   echo $sh_name
+   echo $sql_query
+
 #check to see if there are 2 arguments attached to omni command
-if [ "$#" -lt 4 ]
+if [ "$#" -lt 5 ]
 then
-    echo -e "Not enough arguments supplied, please provide 3 arguments. Exiting..."
+    echo -e "Not enough arguments supplied, please provide 5 arguments. Exiting..."
     exit
 fi
 
@@ -32,35 +46,17 @@ echo $?
 
 if [ $? -eq 1 ]; then
    ## update database from here
-   source ../db_credential
-   user_id=$3
-   slice_name=$1
-   module_id=$4
-   sh_name="create sliver"
 
    echo $user_id
    echo $slice_name
    echo $module_id
    echo $sh_name
 
-   mysql -u$USERNAME -p$PASSWORD -h$SERVER cyberrange -e "INSERT INTO `geni_output` (`slice_name`,`output`,`status`,`user_id`,`module_id`,`sh_name`) VALUES($1,$output,'Success',$user_id,$module_id,'create sliver');" || checkErr "Sql success update"
    echo "database updated with successful status..."
 
 else 
    ## update database from here
-   source ../db_credential
-   user_id=$3
-   slice_name=$1
-   module_id=$4
-   sh_name="create sliver"
 
-   echo $user_id
-   echo $slice_name
-   echo $module_id
-   echo $sh_name
-
-
-   mysql -u$USERNAME -p$PASSWORD -h$SERVER cyberrange -e "INSERT INTO `geni_output` (`slice_name`,`output`,`status`,`user_id`,`module_id`,`sh_name`) VALUES($1,$output,'Failed',$user_id,$module_id,'create sliver');" || checkErr "Sql failed update"
-
+   mysql -u$USERNAME -p$PASSWORD -h$SERVER cyberrange -e "set @slice_name=\"${1}\"; set @output=\"${output}\"; set @status=\"failed\"; set @user_id=\"${user_id}\"; set @module_id=${module_id}; set @sh_name=\"create sliver\"; source ${sql_query};" || checkErr "Sql failed update"
    echo "database updated with fail status..."
 fi
